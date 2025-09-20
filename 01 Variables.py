f743d9177e20c009fdc20843d7b42c077491ca68
jpbc <jpbc80@gmail.com>
2025-09-20 20:03:22 -0300

    Variables: Operaciones

--- 01 Variables.py
+++ 01 Variables.py
@@ -5,3 +5,10 @@ print(a,b,c)
 #Asignacion de multiples variables en una sola linea  
 d,e,f = 5,3.2,'Hola 2'
 print (d,e,f)
+numero =10
+numero_2 = 10
+#sumar Variables
+resultado = numero+numero_2
+print(numero_2)
+print(numero)
+print(resultado)

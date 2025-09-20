a2d4df60b366055aa82484c3c00467723261cbfe
jpbc <jpbc80@gmail.com>
2025-09-20 20:23:06 -0300

    Variables: concatenando texto

--- 01 String.py
+++ 01 String.py
@@ -1,2 +1,10 @@
 nombre = 'Bryan'
-print (nombre)
+#print (nombre)
+
+#Para usar comilla dentro de otras debemos poner diferentes por fuera
+#texto = 'Hola me gusta muchi ser "Programador"'
+#print(texto)
+
+# La funcion print no solo nos muestra variable sino que tambien nos muestra texto
+print('Mi nombre es:',nombre)
+# para concatenar dentro de la funcion print debemos usar la , o el operador +

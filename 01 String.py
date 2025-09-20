0464892ddf796e0b545d04ae210a323eed3c9461
jpbc <jpbc80@gmail.com>
2025-09-20 20:29:53 -0300

    Variables: ver tipos de variables y operadores especiales

--- 01 String.py
+++ 01 String.py
@@ -1,4 +1,4 @@
-nombre = 'Bryan'
+#nombre = 'Bryan'
 #print (nombre)
 
 #Para usar comilla dentro de otras debemos poner diferentes por fuera
@@ -6,5 +6,19 @@ nombre = 'Bryan'
 #print(texto)
 
 # La funcion print no solo nos muestra variable sino que tambien nos muestra texto
-print('Mi nombre es:',nombre)
+#print('Mi nombre es:',nombre)
 # para concatenar dentro de la funcion print debemos usar la , o el operador +
+
+# El salto de linea se usa con \n y el tabulador con \t
+texto ='Hola desde mi variable \n \t\t\t\t texto'
+print(texto)
+
+
+# como ver que tipo de dato es una variable
+
+edad = 45
+nombre = 'Bryan'
+dni =1.0
+print(type(edad))
+print(type(nombre))
+print(type(dni))

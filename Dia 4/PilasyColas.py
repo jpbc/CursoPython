5129af00a43a3263900e77990318f323b708a8ed
jpbc <jpbc80@gmail.com>
2025-09-20 23:21:17 -0300

    Manejo de Prilas y Colas

---
+++ Dia 4/PilasyColas.py
@@ -0,0 +1,12 @@
+fila = ['Jose', 'Maria', 'Juan', 'Carla']
+
+# agregar un valor 
+fila.append ('Romina')
+
+#imprimir fila
+print (fila)
+
+i = fila.pop(0)
+print(f'Estan atendiendo a {i}')
+
+print (fila)

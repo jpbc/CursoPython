03c93a8df2f75e77c89b2d9ddc452bd6837cb856
jpbc <jpbc80@gmail.com>
2025-09-20 22:46:21 -0300

    Manejo de listas comandos del, insert, append

--- Dia 4/Listas.py
+++ Dia 4/Listas.py
@@ -4,4 +4,13 @@ personas=['Martin', 'Maria', 'Gonzalo', ' Raul', 'Pamela']
 # con el metodo del eliminamos a Maria y Gonzalo ocupara la posición 1
 del personas[1]
 
+
+# agregar nombres a la lista indicando la posicion con insert
+personas.insert(0, 'Matias')
+
 print(personas[1])
+
+# agregando elementos en el final de la lista con append
+personas.append ('Jean')
+
+print(personas)

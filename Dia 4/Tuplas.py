f41c72e6224561e6a1cb370f2844a5fc71fd45e0
jpbc <jpbc80@gmail.com>
2025-09-20 23:09:02 -0300

    Manejo de tuplas comandos index, count, len

---
+++ Dia 4/Tuplas.py
@@ -0,0 +1,11 @@
+tupla=(11, 'Python', True, [1,2,3], 'Python')
+
+# imprimir elemento de una tupla a partir del indice 1
+print (tupla [1:])
+
+# En que indice se encuentra el valor Python
+print(tupla.index('Python'))
+
+# Veces que aparece Python en la tupla
+print(tupla.count('Python'))
+print(len(tupla[4]))

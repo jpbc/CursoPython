26dc05b996bb10fc3a0b857788c67dbbaa474a6f
jpbc <jpbc80@gmail.com>
2025-09-20 23:15:07 -0300

    Manejo de Diccionarios

---
+++ Dia 4/Diccionario.py
@@ -0,0 +1,18 @@
+personas ={'Brian':26, 'Maria':22, 'Martin':66, 'Patricia':55}
+
+
+#Acceder a un valor a traves de la clave
+print(personas['Brian'])
+
+# agregamos un valor, tambien podemos actualizar de la misma manera
+
+personas['Jose']=44
+
+personas['Maria']=33
+
+# Eliminando una clave
+
+del personas['Martin']
+
+# imprimiendo el dicccionario
+print(personas)

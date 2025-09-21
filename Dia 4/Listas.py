0167a7ab4509d48b182b6eedd4a60397950f592f
jpbc <jpbc80@gmail.com>
2025-09-20 22:43:14 -0300

    Manejo de listas

---
+++ Dia 4/Listas.py
@@ -0,0 +1,7 @@
+# Manejo de listas, accediendo por su indice todos comienzas desde el 0
+personas=['Martin', 'Maria', 'Gonzalo', ' Raul', 'Pamela']
+
+# con el metodo del eliminamos a Maria y Gonzalo ocupara la posición 1
+del personas[1]
+
+print(personas[1])

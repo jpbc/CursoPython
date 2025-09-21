042ee5e9b14f85062ce124128cc7ad4d1d71625e
jpbc <jpbc80@gmail.com>
2025-09-21 10:59:34 -0300

    Envio Valores

---
+++ Dia 5/EnvioValores.py
@@ -0,0 +1,9 @@
+def sumar(op1,op2):
+    resultado = op1+op2
+    return resultado
+#se pasan los paramentros op1 y op2 
+
+op1 = input ("ingrese el valor de op1")
+op2 = input ("ingrese el valor de op2")
+print(sumar(op1,op2))
+#la impresion muestra el resultado de la suma
